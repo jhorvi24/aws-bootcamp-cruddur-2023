b0c6d351c11e2455247262259258915319758928
Jhorman Villanueva <jhorvi24@hotmail.com>
2023-05-05 17:16:35 +0000

    trying to fix the error about cognito id

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -1,8 +1,8 @@
 -- this file was manually created
 INSERT INTO public.users (display_name, email, handle, cognito_user_id)
 VALUES
-  ('Jhorman Villanueva', 'jhorvi24@hotmail.com','jhorvi24' ,'MOCK'),
-  ('Andrew Bayko', 'jhorvi24+bayko@hotmail.com','bayko' ,'MOCK');
+  ('Jhorman Villanueva', 'jhorvi24@hotmail.com','jhorvi24' ,'4e656cc4-de82-4e87-9228-6f0a40588c18'),
+  ('Andrew Bayko', 'jhorvi24+bayko@hotmail.com','bayko' ,'57342ab4-03a6-441c-88c6-03dfe3911123');
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
 VALUES
